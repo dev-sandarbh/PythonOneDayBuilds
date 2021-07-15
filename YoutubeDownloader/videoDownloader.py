4a757885753e73d4861a8cddff8e28300a1e49b8
dev-sandarbh <tiwari.sandarbh2603@gmail.com>
2021-07-16 02:10:30 +0530

    Added Threading Code

--- YoutubeDownloader/videoDownloader.py
+++ YoutubeDownloader/videoDownloader.py
@@ -4,9 +4,19 @@ from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
 from pytube import *
+from threading import *
+
+# variable to hold our file size
+file_size = 0
+
+def calcProgressDownload(stream, chunk, bytes_remaining):
+    ''' this function calculates the download progress as %age of file downloaded '''
+    percent = round((1-bytes_remaining/file_size)*100, 3)
+    downBtn.config(text="{} % downloaded..".format(percent))
 
 def startDownload():
     ''' This function starts the download and save it to a specified path. '''
+    global file_size
     try:
         urlVal = urlEntry.get()
         
@@ -24,12 +34,17 @@ def startDownload():
         # creating object of YouTube Class
         utubeObj = YouTube(urlVal)
         
+        utubeObj.register_on_progress_callback(calcProgressDownload)
         # selecting first stream out of all available streams
         mainStream = utubeObj.streams.first()
         
+        # to get file size
+        file_size = mainStream.filesize
+        print("Size of the file to be downloaded: ", file_size)
+
         # downloading the selected stream
         mainStream.download(path_to_save)
-        
+
         # changing state, text and bgcolor of button again after downloading gets complete
         downBtn.config(text="Start Again..")
         downBtn.config(state=NORMAL)
@@ -42,11 +57,14 @@ def startDownload():
         showinfo("Download Status", "Downloaded Successfully")
         print("Successfully Downloaded Video....")
 
-
-
     except Exception as e:
         print(e)
         print("Sorry Video Cannot be downloaded.")
+
+def startDownloadThread():
+    ''' Function that starts the Downloading Thread '''
+    dThread = Thread(target=startDownload)
+    dThread.start()
 
 ''' Creating GUI '''
 root = Tk()
@@ -61,7 +79,7 @@ introLabel.pack(side='top')
 urlEntry = Entry(root, font=('Verdana', 15), justify='center')
 urlEntry.pack(side='top', fill='x', padx=75)
 
-downBtn = Button(root, text="Click To Download..", font=('Verdana', 15), foreground='#ffffff', background='#d4483f', relief='ridge', command=startDownload)
+downBtn = Button(root, text="Click To Download..", font=('Verdana', 15), foreground='#ffffff', background='#d4483f', relief='ridge', command=startDownloadThread)
 downBtn.pack(side='top', pady=25)
 
 if __name__ == "__main__":
